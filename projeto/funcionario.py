46c834f50b8a7da77ac16c47fa224f719e9c5ca4
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2024-09-30 12:49:11 +0000

    funcionario v5

---
+++ projeto/funcionario.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from models.endereco import Endereco
+class Funcionario(ABC):
+    def __init__(self,nome: str, telefone: str, email: str, endereco: Endereco, salario_final: float) -> None:
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.endereco = endereco
+        self.salario_final = salario_final
+
+    @abstractmethod
+    def calculo_salario():
+        pass
+
+    def __str__(self) -> str:
+        return (
+            (f"Nome: {self.nome}")
+            (f"\nTelefone: {self.telefone}")
+            (f"\nEmail: {self.email}")
+            (f"\nSalário{self.salario_final}")
+            (f"\n{self.endereco}")
+        )
