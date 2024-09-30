b19be74a5eb80cf8dbbdba7f93613931fd5597f2
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2024-09-30 14:17:59 +0000

    up

---
+++ projeto/models/medico.py
@@ -0,0 +1,10 @@
+from models.funcionario import Funcionario
+from models.endereco import Endereco
+
+class Medico(Funcionario):
+    def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, salario_final: float, crm: str) -> None:
+        super().__init__(nome, telefone, email, endereco, salario_final)
+        self.crm = crm
+
+    def __str__(self) -> str:
+        return f"\n{super().__str__()}\nCRM: {self.crm}"
