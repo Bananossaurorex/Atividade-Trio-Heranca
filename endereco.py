674d2de5fb323a300c167e331245e5329610e4e6
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2024-09-30 12:39:49 +0000

    up

--- endereco.py
+++
@@ -1,9 +0,0 @@
-import 
-
-class Endereco:
-    def __init__(self, logadouro:str, numero:int, complemento:str, cep:int, cidade:str) -> None:
-        self.logaouro = logadouro
-        self.numero = numero
-        self.complemento = complemento
-        self.cep = cep
-        self.cidade = cidade
---
+++ projeto/endereco.py
@@ -0,0 +1,19 @@
+import 
+
+class Endereco:
+    def __init__(self, logadouro:str, numero:int, complemento:str, cep:int, cidade:str) -> None:
+        self.logaouro = logadouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+    def __str__(self) -> str:
+        return(
+        f"Logadouro: {self.logaouro}"
+        f"Complemento: {self.logaouro}"
+        f"Logadouro: {self.logaouro}"
+        f"Logadouro: {self.logaouro}"
+        f"Logadouro: {self.logaouro}"
+        
+        )
