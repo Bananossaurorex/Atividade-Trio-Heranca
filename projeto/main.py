61a00348d837d81ac00d4f7361cff8dd5a96adc3
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2024-09-30 14:22:17 +0000

    updates3.0

--- projeto/main.py
+++ projeto/main.py
@@ -1,1 +1,13 @@
-print("principal")
+import os
+from models.endereco import Endereco
+from models.engenheiro import Engenheiro
+
+
+os.system("clear||cls")
+
+engenheiro1 = Engenheiro("Breno","4002-8922","brenin@gmail.com",
+                         Endereco("R dos uruguaios",333,"Perto da Madre teresa","546321000","Salvador")
+                         ,2500.01,"n sei")
+
+
+print(engenheiro1)
