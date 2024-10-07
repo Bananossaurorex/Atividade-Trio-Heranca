24009b1f917c0028738b98c73785e8c8f02e50cb
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2024-10-07 11:25:00 +0000

    up

--- projeto/models/endereco.py
+++ projeto/models/endereco.py
@@ -1,6 +1,6 @@
 class Endereco:
     def __init__(self, logadouro:str, numero:int, complemento:str, cep:int, cidade:str) -> None:
-        self.logaouro = logadouro
+        self.logadouro = logadouro
         self.numero = numero
         self.complemento = complemento
         self.cep = cep
@@ -8,10 +8,10 @@ class Endereco:
 
     def __str__(self) -> str:
         return(
-        f"Logadouro: {self.logaouro}"
-        f"Complemento: {self.logaouro}"
-        f"Logadouro: {self.logaouro}"
-        f"Logadouro: {self.logaouro}"
-        f"Logadouro: {self.logaouro}"
+        f"Logadouro: {self.logadouro}"
+        f"Número: {self.numero}"
+        f"Complemento: {self.complemento}"
+        f"Cep: {self.cep}"
+        f"Cidade: {self.cidade}"
         
         )
