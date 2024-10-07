83836406a5ce49bbd6273bbf8171cd7afe5c5890
Bananossaurorex <160606050+Bananossaurorex@users.noreply.github.com>
2024-10-07 11:41:49 +0000

    up

--- projeto/pytest/test_engenheiro.py
+++ projeto/pytest/test_engenheiro.py
@@ -2,9 +2,10 @@ import pytest
 from projeto.models.engenheiro import Engenheiro
 from projeto.models.endereco import Endereco
 
+
 @pytest.fixture
 def criar_engenheiro():
-    Engenheiro("Maria","40028922","maria@gmail.com",Endereco("Rua da Maluca",22,"1° andar",23232,"Salvador"),600.3,"Crea")
+    return  Engenheiro("Maria","40028922","maria@gmail.com",Endereco("Rua da Maluca",22,"1° andar",23232,"Salvador"),600.3,"Crea")
 
 
 def test_nome_engenheiro(criar_engenheiro):
